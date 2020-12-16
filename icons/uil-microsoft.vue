dadc0ca7662b9f40b307544201186228e4bc988b
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-12-16 06:44:08 +0000

    Auto-committed changes by create-pull-request action

---
+++ icons/uil-microsoft.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    v-bind="$attrs"
+    :width="size"
+    :height="size"
+    xmlns="http://www.w3.org/2000/svg"
+    class="ui-svg-inline"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path
+      d="M2,22h9.5v-9.5H2V22z M2,11.5h9.5V2H2V11.5z M12.5,2v9.5H22V2H12.5z M12.5,22H22v-9.5h-9.5V22z"
+    />
+  </svg>
+</template>
+
+<script>
+import '../utils/style.css'
+
+export default {
+  props: {
+    size: {
+      type: String,
+      default: '1em'
+    }
+  }
+}
+</script>
